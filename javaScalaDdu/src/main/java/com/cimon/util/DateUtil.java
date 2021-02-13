d76552ca507dadb567ca4f14666c3e06e94e967a
chenrenbing <chenrenbing@meituan.com>
2021-02-13 16:22:05 +0800

    add fix

--- javaScalaDdu/src/main/java/com/cimon/util/DateUtil.java
+++ javaScalaDdu/src/main/java/com/cimon/util/DateUtil.java
@@ -1,4 +1,84 @@
 package com.cimon.util;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateUtil {
+    public static DateFormat getDateFormat(String format){
+        return new SimpleDateFormat(format);
+    }
+    public static DateFormat getDateFormat(){
+        return getDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+    /**
+     * input :yyyy-mm-dd HH:mm:ss 格式的字符串
+     * output : unixstamp 时间戳 ms
+     * */
+    public static long Date2Timestamp(String ts){
+        Timestamp timestamp = Timestamp.valueOf(ts);
+        return timestamp.getTime();
+    }
+    /**
+     * 输入是ms
+     * */
+    public static String timestamp2Date(Long ts){
+        Timestamp timestamp = new Timestamp(ts);
+        return getDateFormat().format(timestamp);
+    }
+    /**
+     * 从时间戳中获取到 一天的小时
+     * */
+    public static int getHourFromTs(Long ts){
+        Date date = new Date(ts);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.HOUR_OF_DAY;
+    }
+    /**
+     * 从时间戳中获取到 小时的分钟
+     * */
+    public static int getMinuteFromTs(Long ts){
+        Date date = new Date(ts);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.MINUTE;
+    }
+
+    /**
+     * 日期 增减天数
+     * */
+    public static String addDays(Long ts,int day){
+        Date date = new Date(ts);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date); // set的是Date类
+        calendar.add(Calendar.DAY_OF_YEAR,day);
+        return getDateFormat().format(calendar.getTime()); // get的也是Date类
+    }
+    /**
+     * 输入格式,默认yyyy-MM-dd
+     * */
+    public static String addDays(String date,int day) throws ParseException{
+
+        String format = "yyyy-MM-dd";
+        return addDays(date,day,format);
+    }
+    public static String addDays(String dt,int day, String format ) throws ParseException {
+        Date date = getDateFormat(format).parse(dt);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date); // set的是Date类
+        calendar.add(Calendar.DAY_OF_YEAR,day);
+        return getDateFormat(format).format(calendar.getTime()); // get的也是Date类
+    }
+
+    /**
+     * 生成日期范围
+     * */
+
 }
---
+++ javaScalaDdu/src/test/java/com/cimon/util/TestDateUtil.java
@@ -0,0 +1,14 @@
+package com.cimon.util;
+import org.junit.Test;
+
+
+public class TestDateUtil {
+
+    @Test
+    public void testTimestampToDate() throws  Exception{
+        System.out.println(DateUtil.timestamp2Date(1613192828000L));
+        System.out.println(DateUtil.Date2Timestamp("2021-02-13 13:07:08"));
+        System.out.println(DateUtil.addDays(1613192828000L,10));
+        System.out.println(DateUtil.addDays("2021-02-13",10));
+    }
+}
